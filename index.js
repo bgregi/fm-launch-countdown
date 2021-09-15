f0a675730552477382c6d7bd4cb6d75588bb0e95
Bruno Geraldi <brunogeraldieng@gmail.com>
2021-09-14 21:12:17 -0300

    Final commit

--- index.js
+++ index.js
@@ -1,29 +1,47 @@
-let launchDate = new Date().getTime() + (1000*60*60*24*99)
+let launchDate = new Date().getTime() + (1000 * 60 * 60 * 24 * 99)
 
 // Update the count down every 1 second
-let x = setInterval(function() {
+let x = setInterval(function () {
 
-  // Get today's date and time
-  let now = new Date().getTime();
-
-  
-  // Find the distance between now and the count down date
-  let distance = launchDate - now;
+    // Get today's date and time
+    let now = new Date().getTime();
 
 
-  // Time calculations for days, hours, minutes and seconds
-  let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    // Find the distance between now and the count down date
+    let distance = launchDate - now;
 
-  // Display the results in each div
-  document.getElementById("days").innerHTML = days
-  document.getElementById("hours").innerHTML = hours
-  document.getElementById("minutes").innerHTML = minutes
-  document.getElementById("seconds").innerHTML = seconds
 
-}, 1000);
+    // Time calculations for days, hours, minutes and seconds
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-console.log(Date.parse(new Date()))
-console.log(new Date().getTime())
+    // Display the results in each div
+    document.getElementById("days").innerHTML = days
+    document.getElementById("hours").innerHTML = hours
+    document.getElementById("minutes").innerHTML = minutes
+    document.getElementById("seconds").innerHTML = seconds
+
+}, 1000)
+
+
+let iconFb = document.getElementById("facebook")
+let iconIg = document.getElementById("instagram")
+let iconPinterest = document.getElementById("pinterest")
+
+
+function iconHighlight(event) {
+    event.target.src = "images/icon-" + event.target.id + "-red.svg"
+}
+function iconHighlightOut(event) {
+    event.target.src = "images/icon-" + event.target.id + ".svg"
+}
+
+iconFb.addEventListener("mouseover", iconHighlight)
+iconIg.addEventListener("mouseover", iconHighlight)
+iconPinterest.addEventListener("mouseover", iconHighlight)
+
+iconFb.addEventListener("mouseleave", iconHighlightOut)
+iconIg.addEventListener("mouseleave", iconHighlightOut)
+iconPinterest.addEventListener("mouseleave", iconHighlightOut)
